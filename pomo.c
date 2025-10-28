7a7e360d1ae9659c4a4c0284b237d78fd5a55c1d
archied27 <archiedolman@gmail.com>
2025-10-28 23:29:53 +0000

    formatting of timer fixed

--- pomo.c
+++ pomo.c
@@ -7,9 +7,15 @@ void startTimer(int t)
     while (t>0)
     {
         if(t<60)
-            printf("\r%ds", t);
+            if(t<10)
+                printf("\r%ds     ", t);
+            else
+                printf("\r%ds    ", t);
         else
-            printf("\r%dm %ds", t/60, t%60);
+            if(t%60<10)
+                printf("\r%dm %ds ", t/60, t%60);
+            else
+                printf("\r%dm %ds", t/60, t%60);
         fflush(stdout);
 
         stop = clock() + CLOCKS_PER_SEC;
